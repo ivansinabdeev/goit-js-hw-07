332c123d0a89dd1b62c2356f2e7ca95fcd4000de
ivansinabdeev <ivansinabdeev@gmail.com>
2021-05-20 00:17:16 +0200

    begin lesson

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const categoryEl = document.querySelector('#categories')
+console.log(categoryEl)
+const itemEl = document.querySelectorAll('.item')
+console.log(itemEl)
+const quantityItems = 
+    itemEl.length;
+    
+//return "В списке ${quantityItems} категорий."
+
+console.log(quantityItems)
